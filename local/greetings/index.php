f65ab5d303a6cdb959b2272105de91dc666d57f0
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2024-11-30 08:46:46 +1300

    Display greetings to user

---
+++ local/greetings/index.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * TODO describe file index
+ *
+ * @package    local_greetings
+ * @copyright  2024 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+require_login();
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+
+$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
+$PAGE->set_pagelayout('standard');
+
+
+$PAGE->set_title(get_string('pluginname', 'local_greetings'));
+$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
+
+
+echo $OUTPUT->header();
+
+if (isloggedin()) {
+    echo html_writer::tag('h3', 'Greetings, ' . fullname($USER), [
+        'class' => 'ajr_class',
+    ]);
+} else {
+    echo html_writer::tag('h3', 'Greetings user', [
+        'class' => 'ajr_class',
+    ]);
+}
+
+
+
+
+echo $OUTPUT->footer();
