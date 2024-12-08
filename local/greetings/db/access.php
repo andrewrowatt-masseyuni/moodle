e75efc8df65c38cb7fdff3bd58073cd7c5d7372d
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2024-12-08 15:31:18 +1300

    Deleted Snap theme.

--- local/greetings/db/access.php
+++ local/greetings/db/access.php
@@ -1,3 +1,4 @@
+<?php
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -14,23 +15,33 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Javascript to initialise the myoverview block.
+ * Capability definitions for Greetings
  *
- * @copyright Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * Documentation: {@link https://moodledev.io/docs/apis/subsystems/access}
+ *
+ * @package    local_greetings
+ * @category   access
+ * @copyright  2024 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-import * as View from 'theme_snap/block_myoverview/block_myoverview_view';
-import * as ViewNav from 'theme_snap/block_myoverview/block_myoverview_view_nav';
+defined('MOODLE_INTERNAL') || die();
 
-/**
- * Initialise all of the modules for the overview block.
- *
- * @param {object} root The root element for the overview block.
- */
-export const init = (root) => {
-    // Initialise the course navigation elements.
-    ViewNav.init(root);
-    // Initialise the courses view modules.
-    View.init(root);
-};
+$capabilities = [
+    'local/greetings:postmessages' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_ALLOW,
+        ],
+    ],
+    'local/greetings:viewmessages' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_ALLOW,
+        ],
+    ],
+];
