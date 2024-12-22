46e1e94ea42398efb5ce901e4d9205a40065f1e0
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2024-12-22 13:24:26 +1300

    Basic block

---
+++ blocks/greetings/edit_form.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Form for editing greetings block instances.
+ *
+ * @package     block_greetings
+ * @copyright   2024 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_greetings_edit_form extends block_edit_form {
+
+    /**
+     * Extends the configuration form for block_greetings.
+     *
+     * @param MoodleQuickForm $mform The form being built.
+     */
+    protected function specific_definition($mform) {
+
+        // Section header title.
+        $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
+
+        $mform->addElement('text', 'config_title', get_string('configtitle', 'block_greetings'));
+        $mform->setType('config_title', PARAM_TEXT);
+
+        $mform->addElement('text', 'config_text', get_string('configtext', 'block_greetings'));
+        $mform->setType('config_text', PARAM_TEXT);
+    }
+}
